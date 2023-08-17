80ef1e3861169f247bec1685f5271ab57878e386
rehan012 <rehan24996@gmail.com>
2023-08-17 14:09:10 +0530

    minute changes

--- client/src/action/index.js
+++ client/src/action/index.js
@@ -20,7 +20,7 @@ export const initializeUserOrdersAC = () => {
         axios.get("https://servermegamarket.onrender.com/userorders").then(function (response) {
             if (response.data.status) {
                 //console.log(response.data);
-                dispatch({ type: INIT_USER_ORDERS, payload: response.data.result })
+                dispatch({ type: INIT_USER_ORDERS, payload: response.data.res })
             }
         })
             .catch(function (error) {
